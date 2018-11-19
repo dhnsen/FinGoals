8ff5d67b5f14321b6d72a3c8ebbcf33f1c68e0e0
Dan <spag1024@hotmail.com>
2018-11-18 18:40:06 -0600

    rebuilt project minus API and views

---
+++ FinGoals/Models/Goal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FinGoals.Models
+{
+    public class Goal
+    {
+        [Required]
+        public ApplicationUser User { get; set; }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public Decimal Amount { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public bool IsActive { get; set; } = true;
+        
+    }
+}
