beaaec3ff8d289d82ffc37158203b274ba1d8c19
Dan <spag1024@hotmail.com>
2018-11-19 06:39:34 -0600

    worked on goalscontroller.create port method. need to copy form previous project

--- FinGoals/Controllers/GoalsController.cs
+++ FinGoals/Controllers/GoalsController.cs
@@ -1,4 +1,5 @@
 ﻿using FinGoals.Models;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,7 @@ using System.Web.Mvc;
 
 namespace FinGoals.Controllers
 {
+    [Authorize]
     public class GoalsController : Controller
     {
 
@@ -21,6 +23,21 @@ namespace FinGoals.Controllers
 
 
             return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Goal formGoal)
+        {
+            var userId = User.Identity.GetUserId();
+            var goal = new Goal
+            {
+                User = _context.Users.Single(u => u.Id == userId),
+                DueDate = formGoal.DueDate,
+                Amount = formGoal.Amount,
+                Name = formGoal.Name
+            };
+            _context.Goals.Add(goal);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
