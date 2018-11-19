6ff7d56c751286f1054014a76cba90778ee3106d
Dan <spag1024@hotmail.com>
2018-11-19 06:13:05 -0600

    re-added create view

---
+++ FinGoals/Controllers/GoalsController.cs
@@ -0,0 +1,26 @@
+﻿using FinGoals.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinGoals.Controllers
+{
+    public class GoalsController : Controller
+    {
+
+        private ApplicationDbContext _context;
+
+        public GoalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public ActionResult Create()
+        {
+
+
+            return View();
+        }
+    }
+}
