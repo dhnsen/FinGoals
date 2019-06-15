be6ecade3e1944fe362ce5719024820e641d15e7
Dan H <spag1024@hotmail.com>
2019-06-15 06:13:34 -0500

    created savingsAmount class using notepad. have not implemented it yet because waiting on Visual Studio to install

---
+++ FinGoals/Models/SavingsAmount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FinGoals.Models
+{
+    public class SavingsAmount
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string ApplicationUserId { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+    }
+}
