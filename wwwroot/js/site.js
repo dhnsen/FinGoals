7ff5e5c4ffc9ccb70db0e45065d7d20f400d759d
Dan H <spag1024@hotmail.com>
2019-08-03 13:48:04 -0500

    working on form to update SavingsAmount

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,35 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+function editItem(id) {
+    $.each(todos, function (key, item) {
+        if (item.id === id) {
+            $("#edit-name").val(item.name);
+            $("#edit-id").val(item.id);
+            $("#edit-isComplete")[0].checked = item.isComplete;
+        }
+    });
+    $("#spoiler").css({ display: "block" });
+}
+
+$(".my-form").on("submit", function () {
+    const item = {
+        name: $("#edit-name").val(),
+        isComplete: $("#edit-isComplete").is(":checked"),
+        id: $("#edit-id").val()
+    };
+
+    $.ajax({
+        url: uri + "/" + $("#edit-id").val(),
+        type: "PUT",
+        accepts: "application/json",
+        contentType: "application/json",
+        data: JSON.stringify(item),
+        success: function (result) {
+            getData();
+        }
+    });
+
+    closeInput();
+    return false;
+});
