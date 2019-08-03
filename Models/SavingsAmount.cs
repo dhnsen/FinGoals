6b2aeecff730b9ee7d14a1005b9f457e3e21f0a8
Dan H <spag1024@hotmail.com>
2019-08-03 10:27:23 -0500

    updated database to include Savings Amount

--- Models/SavingsAmount.cs
+++ Models/SavingsAmount.cs
@@ -8,6 +8,8 @@ namespace FinGoals.Models
 {
     public class SavingsAmount
     {
+        // NOTE: the ID is going to be the ID of the user
+        // Each user will only have one.
         [Key]
         public string Id { get; set; }
         public double Amount { get; set; }
