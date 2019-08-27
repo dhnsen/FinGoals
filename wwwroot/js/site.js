cf5586fff3d1d5df93d96235bf33f599f84dab28
Dan H <spag1024@hotmail.com>
2019-08-26 19:08:38 -0500

    wrote psuedo code steps for creating goal nodes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,6 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
+﻿
 let savingsAmount;
 let savingsToDistribute;
 let totalSavingsRequired;
@@ -29,8 +27,14 @@ function clearGoalsDisplay() {
 }
 
 function writeGoalsDisplay() {
+    // define the parent node
+
     goals.array.forEach(element => {
-        
+        //append a child node to the parent with unique ID
+
+        //create variable for the child node
+
+        //set attributes for the node
     });
 }
 
